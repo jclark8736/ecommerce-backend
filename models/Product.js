c71437c8a42511363d82785fcb77730b40212ff5
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-06-15 23:37:05 -0400

    think models are done

--- models/Product.js
+++ models/Product.js
@@ -4,15 +4,46 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
-
-    
+    id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
   },
+
   {
     sequelize,
     timestamps: false,
