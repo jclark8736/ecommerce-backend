a24105820822714ee1a62458a322aab8e5ba49a4
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-06-21 20:39:26 -0400

    working on tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
-      
+      attributes: ["id", "product_name", "price", "stock", "category_id"]
 
     });
     
@@ -36,6 +36,7 @@ router.get('/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
+      attributes: ["id", "product_name", "price", "stock", "category_id"]
     });
 
     if (!categoryData) {
@@ -55,14 +56,45 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then(categoryData => {
+    if (!categoryData[0]) {
+      res.status(404).json({message: "No category found with this id!"});
+      return;
+    }
+    res.json(categoryData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+      attributes: ["id", "product_name", "price", "stock", "category_id"]
+
+    });
+    
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
